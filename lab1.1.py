9959e93916ee5ee0d60d7985dd12fecd6fc829e2
vladysik03 <90906452+vladysik03@users.noreply.github.com>
2021-11-11 22:22:40 +0300

    Add files via upload

---
+++ lab1.1.py
@@ -0,0 +1,33 @@
+import json
+file=open ('pokemon_full.json')
+file1 = file.read()
+#вывод 1 пункта
+print("Количество символов в файле:",len(file1))
+
+count = 0
+for i in file1:
+    if i.isalnum() == True:
+        count += 1
+#вывод 2 пункта
+print("Количество символов в файле без пробелов и знаков препинания:",count)
+
+pokemon_full_massive = json.loads(file1)
+max_dlina = 0
+pokemon_name = ' '
+for descr in pokemon_full_massive:
+    max_dlina = max(len(descr['description']),max_dlina)
+    if len(descr['description']) == max_dlina:
+        pokemon_name = descr['name']
+#вывод 3 пункта
+print("Длина:", max_dlina, "Покемон:", pokemon_name)
+
+max_dlina = 0
+max_ability = ' '
+for abilities in pokemon_full_massive:
+    for ability in abilities['abilities']:
+        max_dlina = max(len(ability.split()),max_dlina)
+        if max_dlina == len(ability.split()):
+            max_ability = ability
+#вывод 4 пункта
+print("Умение: ", max_ability)
+file.close()
