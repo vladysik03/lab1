288677609f58695ffe66973feb3f194e8d05fabc
vladysik03 <90906452+vladysik03@users.noreply.github.com>
2021-11-25 14:26:19 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,33 +1,33 @@
 import json
 
-file=open ('pokemon_full.json')
-pokemon_string = file.read ()
+file = open('pokemon_full.json')
+pokemon_string = file.read()
 #вывод 1 пункта
-print ('Количество символов в файле:',len (pokemon_string))
+print('Количество символов в файле:', len(pokemon_string))
 
 count = 0
 for symbol in pokemon_string:
-    if symbol.isalnum ():
+    if symbol.isalnum():
         count += 1
 #вывод 2 пункта
-print ('Количество символов в файле без пробелов и знаков препинания:',count)
+print('Количество символов в файле без пробелов и знаков препинания:', count)
 
-pokemon_full_list = json.loads (pokemon_string)
+pokemon_full_list = json.loads(pokemon_string)
 max_lenght = 0
 pokemon_name = ' '
 for descr in pokemon_full_list:
-    max_lenght = max (len (descr['description']), max_lenght)
-    if len (descr['description']) == max_lenght:
+    max_lenght = max(len(descr['description']), max_lenght)
+    if len(descr['description']) == max_lenght:
         pokemon_name = descr['name']
 #вывод 3 пункта
-print ('Длина:', max_lenght, 'Покемон:', pokemon_name)
+print('Длина:', max_lenght, 'Покемон:', pokemon_name)
 max_lenght = 0
 max_ability = ' '
 for abilities in pokemon_full_list:
     for ability in abilities['abilities']:
-        max_lenght = max (len (ability.split ()), max_lenght)
-        if max_lenght == len (ability.split ()):
+        max_lenght = max(len(ability.split()), max_lenght)
+        if max_lenght == len(ability.split()):
             max_ability = ability
 #вывод 4 пункта
-print ('Умение: ', max_ability)
-file.close ()
+print('Умение: ', max_ability)
+file.close()
